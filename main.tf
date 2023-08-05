95a77ad10d41ce2e5b13924dee232a938c69a469
choigonyok <achoistic98@naver.com>
2023-08-06 05:19:47 +0900

    feat : add remote-exec inline to config k8s cluster

--- main.tf
+++ main.tf
@@ -74,12 +74,54 @@ resource "aws_instance" "master_node" {
     host = self.public_ip
   }
 
+
    provisioner "remote-exec" {
 
     inline = [
-      "git clone https://github.com/wardviaene/on-prem-or-cloud-agnostic-kubernetes.git",
-      "cd on-prem-or-cloud-agnostic-kubernetes",
-      "yes | sudo scripts/install-kubernetes.sh",
+      "cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf",
+      "overlay",
+      "br_netfilter",
+      "EOF",
+      "sudo modprobe overlay",
+      "sudo modprobe br_netfilter",
+      "cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf",
+      "net.bridge.bridge-nf-call-iptables  = 1",
+      "net.bridge.bridge-nf-call-ip6tables = 1",
+      "net.ipv4.ip_forward = 1",
+      "EOF",
+      "sudo sysctl --system",
+      "sudo swapoff -a",
+      "(crontab -l 2>/dev/null; echo '@reboot /sbin/swapoff -a') | crontab - || true",
+      "wget https://github.com/containerd/containerd/releases/download/v1.7.3/containerd-1.7.3-linux-amd64.tar.gz",
+      "sudo tar Czxvf /usr/local containerd-1.7.3-linux-amd64.tar.gz",
+      "wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service",
+      "sudo mv containerd.service /usr/lib/systemd/system/",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable --now containerd",
+      "wget https://github.com/opencontainers/runc/releases/download/v1.1.8/runc.amd64",
+      "sudo install -m 755 runc.amd64 /usr/local/sbin/runc",
+      "sudo mkdir -p /etc/containerd/",
+      "containerd config default | sudo tee /etc/containerd/config.toml",
+      "sudo sed -i 's/SystemdCgroup \\= false/SystemdCgroup \\= true/g' /etc/containerd/config.toml",
+      "sudo systemctl restart containerd",
+      "sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl",
+      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
+      "cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "deb https://apt.kubernetes.io/ kubernetes-xenial main",
+      "EOF",
+      "sudo apt-get update",
+      "sudo apt-get install -y kubelet kubeadm kubectl",
+      "sudo apt-mark hold kubelet kubeadm kubectl containerd",
+      "IPADDR=$(curl ifconfig.me && echo \"\")",
+      "NODENAME=$(hostname -s)",
+      "POD_CIDR=\"192.168.0.0/16\"",
+      "touch text.txt",
+      "sudo kubeadm init --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME --ignore-preflight-errors Swap > text.txt",
+      "sudo mkdir -p $HOME/.kube",
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+      "kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml",
+      "kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml",
     ]
   }
 }
@@ -104,11 +146,43 @@ resource "aws_instance" "worker_nodes" {
     host = self.public_ip
   }
    
-  provisioner "remote-exec" {
+provisioner "remote-exec" {
+
     inline = [
-      "git clone https://github.com/wardviaene/on-prem-or-cloud-agnostic-kubernetes.git",
-      "cd on-prem-or-cloud-agnostic-kubernetes",
-      "yes | sudo scripts/install-node.sh",
+      "cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf",
+      "overlay",
+      "br_netfilter",
+      "EOF",
+      "sudo modprobe overlay",
+      "sudo modprobe br_netfilter",
+      "cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf",
+      "net.bridge.bridge-nf-call-iptables  = 1",
+      "net.bridge.bridge-nf-call-ip6tables = 1",
+      "net.ipv4.ip_forward = 1",
+      "EOF",
+      "sudo sysctl --system",
+      "sudo swapoff -a",
+      "(crontab -l 2>/dev/null; echo '@reboot /sbin/swapoff -a') | crontab - || true",
+      "wget https://github.com/containerd/containerd/releases/download/v1.7.3/containerd-1.7.3-linux-amd64.tar.gz",
+      "sudo tar Czxvf /usr/local containerd-1.7.3-linux-amd64.tar.gz",
+      "wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service",
+      "sudo mv containerd.service /usr/lib/systemd/system/",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable --now containerd",
+      "wget https://github.com/opencontainers/runc/releases/download/v1.1.8/runc.amd64",
+      "sudo install -m 755 runc.amd64 /usr/local/sbin/runc",
+      "sudo mkdir -p /etc/containerd/",
+      "containerd config default | sudo tee /etc/containerd/config.toml",
+      "sudo sed -i 's/SystemdCgroup \\= false/SystemdCgroup \\= true/g' /etc/containerd/config.toml",
+      "sudo systemctl restart containerd",
+      "sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl",
+      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
+      "cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "deb https://apt.kubernetes.io/ kubernetes-xenial main",
+      "EOF",
+      "sudo apt-get update",
+      "sudo apt-get install -y kubelet kubeadm kubectl",
+      "sudo apt-mark hold kubelet kubeadm kubectl containerd",
     ]
   }
 }
@@ -124,22 +198,3 @@ output "worker1-ip" {
 output "worker2-ip" {
   value = "${aws_instance.worker_nodes[1].public_ip}"
 }
-
-# master는 
-# nodes는 sudo scripts/install-node.sh
-
-# master에 나온 join 명령어 node에서 실행
-
-# master : vim scripts/create-user.sh
-# 들어가서
-#     useradd => usermod 변경
-#     맨위에 groupadd ubuntu? 삭제
-# result=$(sudo scripts/install-kubernetes.sh | grep -o "hello")
-# "result=$(yes | sudo scripts/install-kubernetes.sh | grep -o 'kubeadm join [^\n]*')"
-# result=$(yes | sudo scripts/install-kubernetes.sh | grep -o '^kubeadm join .*')
-# echo "$result" > ../result.txt,
-
-# vim scripts/create-user.sh
-# sudo scripts/create-user.sh
-
-# sudo kubelet ~
